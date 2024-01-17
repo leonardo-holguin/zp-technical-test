e16a341318abf3a942a5348b767c8a0cce016a68
Leonardo Holguin Arias <leonardo9097@hotmail.com>
2024-01-17 04:05:00 -0500

    feat: SSR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, signal } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,8 +7,61 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'zp-technical-test';
+  isBrowser = false;
+  loading = signal(false);
+  darkMode = signal(true);
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      const currentDarkMode = localStorage.getItem('darkMode');
+      if (currentDarkMode) {
+        this.darkMode.set(currentDarkMode.toLowerCase() === 'true');
+      } else {
+        this.darkMode.set(
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+        );
+      }
+    }
+  }
+
+  toggleDarkMode(): void {
+    const currentValue: boolean = this.darkMode();
+    this.darkMode.set(!currentValue);
+    localStorage.setItem('darkMode', (!currentValue).toString());
+  }
+
+  async getServerData(): Promise<void> {
+    let tries = 0;
+
+    while (tries < 10 && this.loading()) {
+      try {
+        const response = await fetch(
+          'http://pbiz.zonavirtual.com/api/Prueba/Consulta',
+          {
+            method: 'POST',
+            headers: {
+              Accept: '*/*',
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        const json = (await response.json()) as Promise<
+          { comercio_codigo: string }[]
+        >;
+        console.log('entra2');
+        console.log(json);
+        this.loading.set(true);
+      } catch (error) {
+        this.loading.set(false);
+      }
+      tries++;
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  darkMode: 'class',
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Montserrat", "ui-sans-serif", "system-ui"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: "#f0fdf9",
+          100: "#cdfaee",
+          200: "#9bf4de",
+          300: "#61e7cb",
+          400: "#30d1b5",
+          500: "#15a48d",
+          600: "#10917f",
+          700: "#117467",
+          800: "#125d54",
+          900: "#144d46",
+          950: "#052e2b",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
