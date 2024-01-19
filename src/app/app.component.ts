cf4249c6347c1fcffe5897b8e86f201203a51d5b
Leonardo Holguin Arias <leonardo9097@hotmail.com>
2024-01-19 04:48:02 -0500

    feat: Pagination and search impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { IInvoiceServerDTO } from './models/dto/invoice.server.dto';
 import { TrCardComponent } from './components/tr-card/tr-card.component';
 import { IInvoice } from './models/invoice';
-import { PAYMENT_METHOD, TRANSACTION_STATUS } from './models/transaction';
 import { TrFormComponent } from './components/tr-form/tr-form.component';
 import { InvoiceService } from './services/invoice';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TrCardComponent, TrFormComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterOutlet,
+    TrCardComponent,
+    TrFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -22,10 +27,43 @@ export class AppComponent {
   data = signal<IInvoice[] | undefined>(undefined);
   currentData = signal<IInvoice | undefined>(undefined);
 
+  pagPages = signal(0);
+  currentPage = signal(1);
+  searchedData = signal<IInvoice[] | undefined>(undefined);
+  searchInput = new FormControl('');
+
   constructor() {
     void this.darkModeHandler();
     void this.dataHandler();
+
+    this.searchedData.set(this.data());
+    this.searchInput.valueChanges.subscribe((searchValue) => {
+      if (searchValue === null) {
+        return;
+      }
+      const valueLowercase = searchValue.toLowerCase();
+      this.searchedData.set(this.data());
+      this.searchedData.update((dataArray) =>
+        dataArray?.filter(
+          (data) =>
+            data.transactionCode.includes(valueLowercase) ||
+            data.transactionTotal.toString().includes(valueLowercase) ||
+            data.transactionDate.includes(valueLowercase)
+        )
+      );
+      this.pagPages.set(this.setPag(this.searchedData()?.length));
+      this.currentPage.set(1);
+    });
   }
+
+  computedData = computed(() => {
+    const currentPage = this.currentPage();
+    const searchedData = this.searchedData();
+
+
+
+    return searchedData?.slice((currentPage-1)*18, currentPage*18);
+  });
 
   async darkModeHandler() {
     const currentDarkMode = localStorage.getItem('darkMode');
@@ -48,6 +86,15 @@ export class AppComponent {
     this.loading.set(true);
     let resetedData = await InvoiceService._getData();
     this.data.set(resetedData);
+    this.searchedData.set(this.data());
+    this.pagPages.set(this.setPag(this.data()?.length));
     this.loading.set(false);
+  }
+
+  setPag(dataLenght?: number) {
+    if (dataLenght === undefined) {
+      return 1;
+    }
+    return Math.ceil(dataLenght / 18);
   }
 }
